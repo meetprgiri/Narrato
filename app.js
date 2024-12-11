07e9cd8753f430e6f693061a3246838c928b66d5
meetprgiri <157962834+meetprgiri@users.noreply.github.com>
2024-12-11 14:40:10 +0530

    .

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 
 app.listen(
   PORT,
